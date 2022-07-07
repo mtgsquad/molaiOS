f948d4dcf2f2ce4f6343557e0762104a8603bdec
Mahir Molai <mahir@molai.dev>
2022-07-07 12:27:14 +0500

    Change text

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,5 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_str("MolaiOS v1.1 (x64)");
 }
